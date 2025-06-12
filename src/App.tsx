879f2275cc119d5bfc01dfba72a34acd19bfb8f0
RichaPOpenEyes <182314898+RichaPatel-Richawebcreator@users.noreply.github.com>
2025-06-12 18:50:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,19 @@ export const App = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-100 to-blue-300  text-gray-900  p-6">
-      <Flex>
+      <Flex vertical align="center" justify="center">
         <Title className="!text-3xl !text-blue-900">🌤️ Weather Forecast</Title>
+        <div className="flex flex-col md:flex-row items-center gap-4">
+          <Input.Search
+            placeholder="Enter city name"
+            enterButton="Search"
+            size="large"
+            onSearch={handleSearch}
+            className="w-72 mb-8"
+          />
+        </div>
+        {city && <WeatherCard city={city} />}
       </Flex>
-      <div className="flex flex-col md:flex-row items-center gap-4">
-        <Input.Search
-          placeholder="Enter city name"
-          enterButton="Search"
-          size="large"
-          onSearch={handleSearch}
-          className="w-72"
-        />
-      </div>
-
-      {city && <WeatherCard city={city} />}
     </div>
   );
 };
